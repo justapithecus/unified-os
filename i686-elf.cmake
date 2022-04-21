fd35c4d667b16cd594e80c46e1a8e301c16c1347
Andrew Hu <andrew.hu@pithecine.com>
2022-04-21 08:01:25 -0400

    fix toolchain cmake flag setting

--- i686-elf.cmake
+++ i686-elf.cmake
@@ -1,7 +1,7 @@
-ENABLE_LANGUAGE(ASM)
-
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR i686)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE "STATIC_LIBRARY")
 
 set(CMAKE_CXX_COMPILER /opt/bin/i686-elf-g++)
 set(CMAKE_C_COMPILER /opt/bin/i686-elf-gcc)
@@ -9,5 +9,5 @@ set(CMAKE_ASM_COMPILER /opt/bin/i686-elf-as)
 
 set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -ffreestanding -fno-exceptions -fno-rtti")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -ffreestanding")
+string(APPEND CMAKE_CXX_FLAGS "-Wall -Wextra -ffreestanding -fno-exceptions -fno-rtti")
+string(APPEND CMAKE_C_FLAGS "-Wall -Wextra -ffreestanding")
